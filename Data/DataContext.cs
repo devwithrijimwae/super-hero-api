2f7c31080f9a3fa6595a98d3f9faee1668101b3c
Peace Elisha Iratishe <peaceelishairatishe@gmail.com>
2025-11-13 11:41:47 +0100

    Create DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using super_hero_api.Entities;
+
+namespace super_hero_api.Data
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
